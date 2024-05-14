c80c4d7f6bff3cc6a0e89a24784ec8df6f1d0ada
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-05-14 14:03:29 +0700

    Update teslagi.py

--- teslagi.py
+++ teslagi.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+
+
+# Initialize connection.
+# conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
+# conn = st.connection(**st.secrets.db_credentials)
+conn = st.connection("mydb", type="sql", autocommit=True)
+
+# Perform query.
+df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
+
+st.table(df)
+# Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.EnglishPromotionName} , {row.MaxQty} ")
