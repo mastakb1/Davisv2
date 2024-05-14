f3917dc976fc9bc7142f5e946c1ccad8c36a91cc
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-05-14 13:43:18 +0700

    Update texttospeech.py

--- texttospeech.py
+++ texttospeech.py
@@ -1,1 +1,24 @@
+import translators as ts
+import pyttsx3
+import time
 
+engine = pyttsx3.init()
+
+
+
+text = "This company was founded in 2010 by the infamous movie star, \
+          Graeme Alexander. Currently, the company worths USD 1 billion \
+          according to Forbes report in 2023. What an achievement in just \
+          13 years."
+engine.say(text)
+engine.runAndWait()
+time.sleep(2)
+engine.say("terjemahannya dalam bahasa Indonesia sebagai berikut")
+engine.runAndWait()
+time.sleep(2)
+hasil = ts.translate_text(text, to_language="id", translator='google')
+print(hasil)
+time.sleep(1)
+engine.say(hasil)
+engine.runAndWait()
+engine.stop()
